f563d3e9ed64c14c2dccb6749d887ad144925089
kenneth xu <kx123@live.ca>
2017-06-30 20:43:22 -0400

    made templates

--- music/views.py
+++ music/views.py
@@ -7,11 +7,15 @@ from django.http import HttpResponse
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
 
+    #links index to a template
+    template = loader.get_template('music/index.html')
+    context = {
+        "all_albums": all_albums,
+
+    }
+    return HttpResponse(template.render(context, request))
+
+#html reponse to when user clicks link of album title, they will see album id
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " +str(album_id) + "</h2>")
